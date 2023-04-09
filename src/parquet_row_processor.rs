a9c8076d5cb49bd21b601ba1beea5a85884befb0
Per Arneng <per.arneng@scalebit.com>
2023-04-09 13:47:50 +0200

    initial type conversions

--- src/parquet_row_processor.rs
+++ src/parquet_row_processor.rs
@@ -3,6 +3,9 @@ use tokio_postgres::{Column, Row};
 use crate::logging::init;
 use crate::postquet_engine::RowProcessor;
 use std::sync::{Mutex, Once};
+use arrow::datatypes::{DataType, TimeUnit};
+use log::error;
+use tokio_postgres::types::Type;
 
 pub struct ParquetRowProcessor {
     init_once: Once,
@@ -18,8 +21,18 @@ impl ParquetRowProcessor {
 
 impl ParquetRowProcessor  {
     fn init(&self, columns: &[Column]) -> Result<(), Box<dyn Error>> {
-        println!("init {:?}", columns);
+        //println!("init {:?}", columns);
+        for column in columns {
+            //println!("column {:?}", column);
 
+            let arrow_type= to_arrow_type(&column.type_())
+                .ok_or(format!("unsupported postgresql type {:?} for column {}. sorry!",
+                               column.type_(), column.name()))?;
+
+            //println!("type {:?}", column.type_());
+            //println!("type name {:?}", column.type_().name());
+            //println!("arrow type {:?}", arrow_type);
+        }
         Ok(())
     }
 }
@@ -36,5 +49,23 @@ impl RowProcessor for ParquetRowProcessor  {
 
         println!("process_row {:?}", row);
         Ok(())
+    }
+}
+
+fn to_arrow_type(t: &Type) -> Option<DataType> {
+    match t {
+        &Type::BOOL => Some(DataType::Boolean),
+        &Type::CHAR => Some(DataType::Int8),
+        &Type::INT2 => Some(DataType::Int16),
+        &Type::INT4 => Some(DataType::Int32),
+        &Type::INT8 => Some(DataType::Int64),
+        &Type::FLOAT4 => Some(DataType::Float32),
+        &Type::FLOAT8 => Some(DataType::Float64),
+        &Type::TEXT | &Type::VARCHAR | &Type::BPCHAR => Some(DataType::Utf8),
+        &Type::DATE => Some(DataType::Date32),
+        &Type::TIMESTAMP => Some(DataType::Timestamp(TimeUnit::Microsecond, None)),
+        &Type::TIMESTAMPTZ => Some(DataType::Timestamp(TimeUnit::Microsecond, Some("UTC".to_string()))),
+        &Type::BYTEA => Some(DataType::Binary),
+        _ => None, // You may add more conversions here or return None for unsupported types
     }
 }
