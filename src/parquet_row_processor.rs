8c4020518330dc697860c2ee26409edf57a4e970
Per Arneng <per.arneng@scalebit.com>
2023-04-09 13:15:07 +0200

    init runs once

--- src/parquet_row_processor.rs
+++ src/parquet_row_processor.rs
@@ -2,20 +2,24 @@ use std::error::Error;
 use tokio_postgres::{Column, Row};
 use crate::logging::init;
 use crate::postquet_engine::RowProcessor;
+use std::sync::{Mutex, Once};
 
 pub struct ParquetRowProcessor {
-
+    init_once: Once,
 }
 
 impl ParquetRowProcessor {
     pub fn new() -> Self {
-        ParquetRowProcessor {}
+        Self {
+            init_once: Once::new(),
+        }
     }
 }
 
 impl ParquetRowProcessor  {
     fn init(&self, columns: &[Column]) -> Result<(), Box<dyn Error>> {
         println!("init {:?}", columns);
+
         Ok(())
     }
 }
@@ -23,7 +27,13 @@ impl ParquetRowProcessor  {
 impl RowProcessor for ParquetRowProcessor  {
 
     fn process_row(&self, row: &Row) -> Result<(), Box<dyn Error>> {
-        self.init(&row.columns())?;
+
+        let mut init_result = Ok(());
+        self.init_once.call_once(|| init_result = self.init(&row.columns()));
+        if init_result.is_err() {
+            return init_result;
+        }
+
         println!("process_row {:?}", row);
         Ok(())
     }
