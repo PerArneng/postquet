091c8dd64cccd1cfa4e1d14389a4c3ac59a520cb
Per Arneng <per.arneng@scalebit.com>
2023-04-10 12:03:16 +0200

    imporoved conversion of values

--- src/parquet_row_processor.rs
+++ src/parquet_row_processor.rs
@@ -3,9 +3,11 @@ use tokio_postgres::{Column, Row};
 use crate::logging::init;
 use crate::postquet_engine::RowProcessor;
 use std::sync::{Mutex, Once};
-use arrow::datatypes::{DataType, TimeUnit};
+use arrow::datatypes::{DataType, Field, Schema, TimeUnit};
+use chrono::{DateTime, Local, NaiveDateTime, NaiveTime, TimeZone, Utc};
+use futures_util::FutureExt;
 use log::error;
-use tokio_postgres::types::Type;
+use tokio_postgres::types::{FromSql, Type};
 
 pub struct ParquetRowProcessor {
     init_once: Once,
@@ -22,17 +24,8 @@ impl ParquetRowProcessor {
 impl ParquetRowProcessor  {
     fn init(&self, columns: &[Column]) -> Result<(), Box<dyn Error>> {
         //println!("init {:?}", columns);
-        for column in columns {
-            //println!("column {:?}", column);
-
-            let arrow_type= to_arrow_type(&column.type_())
-                .ok_or(format!("unsupported postgresql type {:?} for column {}. sorry!",
-                               column.type_(), column.name()))?;
-
-            //println!("type {:?}", column.type_());
-            //println!("type name {:?}", column.type_().name());
-            //println!("arrow type {:?}", arrow_type);
-        }
+        let schema = create_parquet_schema(&columns)?;
+        println!("schema: {:?}", schema);
         Ok(())
     }
 }
@@ -48,6 +41,34 @@ impl RowProcessor for ParquetRowProcessor  {
         }
 
         println!("process_row {:?}", row);
+        for col_index in 0..row.len() {
+            let column = row.columns().get(col_index).ok_or("column not found")?;
+            let col_type = column.type_();
+
+            if col_type == &Type::INT8 {
+                let value:Option<i64> = row.try_get(col_index)?;
+                println!("value {}::'{}' i64: {:?}", col_type, column.name() ,value);
+            } else if col_type == &Type::INT4 {
+                let value:Option<i32> = row.try_get(col_index)?;
+                println!("value {}::'{}' i32: {:?}", col_type, column.name() ,value);
+            } else if col_type == &Type::VARCHAR {
+                let value: Option<String> = row.try_get(col_index)?;
+                println!("value {}::'{}' String: {:?}", col_type, column.name(), value);
+            } else if col_type == &Type::FLOAT8 {
+                    let value:Option<f64> = row.try_get(col_index)?;
+                    println!("value {}::'{}' f64: {:?}", col_type, column.name() ,value);
+            } else if col_type == &Type::TIMESTAMP {
+                let value:Option<NaiveDateTime> = row.try_get(col_index)?;
+                println!("value {}::'{}' NaiveDateTime: {:?}", col_type, column.name() ,value);
+            } else {
+                return Err(format!("unsupported type '{}' for column '{}'", col_type, column.name()).into());
+            }
+
+            //let value: Result<String, _> = row.get(x);
+            //let value: dyn FromSql = row.try_get(x)
+                                   // .map(|v| v.to_string())?;
+            //println!("column: {:?}, value: {}", column, &value);
+        }
         Ok(())
     }
 }
@@ -68,4 +89,19 @@ fn to_arrow_type(t: &Type) -> Option<DataType> {
         &Type::BYTEA => Some(DataType::Binary),
         _ => None, // You may add more conversions here or return None for unsupported types
     }
+}
+
+pub fn create_parquet_schema(columns: &[Column]) -> Result<Schema, Box<dyn std::error::Error>> {
+    let mut fields = Vec::new();
+
+    for column in columns {
+        let arrow_type= to_arrow_type(&column.type_())
+            .ok_or(format!("unsupported postgresql type {:?} for column {}",
+                           column.type_(), column.name()))?;
+        let field = Field::new(column.name(), arrow_type, true);
+        fields.push(field);
+    }
+
+    let schema = Schema::new(fields);
+    Ok(schema)
 }
